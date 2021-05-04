077454c5a19bc7159d97a3b23e9df9cff1de381d
Martab1 <martabernardo86@gmail.com>
2021-05-04 15:39:17 +0200

    create repo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section>
+      <!-- header -->
+      <Header/>
+
+      <!-- main -->
+      <main>main</main>
+
+      <!-- footer -->
+      <footer>footer</footer>
+    </section>
+    
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// importazione
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // oggetto component
+     Header,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <a href="#">logo</a>
+        <nav>
+            
+        </nav>
+    </header>
+  
+</template>
+
+
+<script>
+export default {
+    // nome componente
+    name: "Header",
+}
+</script>
+
+
+<style scoped>
+
+</style>
